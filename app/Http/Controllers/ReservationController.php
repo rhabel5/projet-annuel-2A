0c061c81575e144b58ac59f501ef284244695a3e
Wakil17 <youssoufwakil@gmail.com>
2024-06-13 01:43:23 +0200

    fixed prix total

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -7,6 +7,7 @@ use App\Models\Reservation;
 use App\Models\Role;
 use App\Models\Role_user;
 use App\Models\User;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
@@ -22,10 +23,16 @@ class ReservationController extends Controller
 
     public function reserver(Request $request){
 
-
+        //Récupère l'id du bien on utilise json decode car dans la requête nous renvoie un json
         $bien = json_decode($request->input('bien_id'));
 
+        //On récupère le nombre de jours
+        $dateDebut = new DateTime($request->input('date_debut'));
+        $dateFin = new DateTime($request->input('date_fin'));
+        $intervale = $dateDebut->diff($dateFin);
+        $jours = $intervale->days;
 
+        $prix_total = $bien->prix_adulte * $jours * $request->input('nombre_adultes');
 
 
 
@@ -36,7 +43,7 @@ class ReservationController extends Controller
         $reservation->date_debut = $request->input('date_debut');
         $reservation->date_fin = $request->input('date_fin');
         $reservation->nb_adulte = $request->input('nombre_adultes');
-        $reservation->prix_total = $bien->prix_adulte * $request->input('nombre_adulte');
+        $reservation->prix_total = $prix_total;
 
         $reservation->save();
     }
