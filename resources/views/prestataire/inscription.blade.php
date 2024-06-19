a042df7a64d8a725e4da5d995ce7f8bd8ed498f1
Wakil17 <youssoufwakil@gmail.com>
2024-06-19 03:30:27 +0200

    selction de type de prestation cote presta

--- resources/views/prestataire/inscription.blade.php
+++ resources/views/prestataire/inscription.blade.php
@@ -1,3 +1,21 @@
+@php
 
-@csrf
+    use App\Models\TypePrestation;
 
+    $typePrestations = TypePrestation::all();
+
+@endphp
+
+<form action="/route-ou-traiter-le-formulaire" method="POST">
+    @csrf
+    @foreach ($typePrestations as $typePrestation)
+        <div class="my-4">
+            <label class="inline-flex items-center">
+                <input type="checkbox" name="type_prestation[]" value="{{ $typePrestation->id }}">
+                <span class="ml-2">{{ $typePrestation->nom }} - {{ $typePrestation->description }} - Prérequis : {{ $typePrestation->prerequis }}</span>
+            </label>
+        </div>
+    @endforeach
+
+    <input type="submit" value="Soumettre" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded shadow">
+</form>
