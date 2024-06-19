ff8d73fc995b3c22bfcebc8cfe529635d48b493b
Wakil17 <youssoufwakil@gmail.com>
2024-06-19 21:26:18 +0200

    choix de type de presta ok

--- app/Http/Controllers/PrestataireController.php
+++ app/Http/Controllers/PrestataireController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bailleur;
+use App\Models\PrestaTypeMission;
 use App\Models\Role_user;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -20,13 +21,39 @@ class PrestataireController extends Controller
 
     public function create(Request $request){
 
- print_r($request->all());
+        $typePrestationIds = $request->input('type_prestation');
 
-        /*$prestataire = User::create($request->all());
-        $roleUser = new Role_user;
-        $roleUser->role_id = 4;
-        $roleUser->user_id = Auth::id();
-        $roleUser->save();*/
+        $missionCount = 0;
+
+        foreach($typePrestationIds as $id) {
+            $mission = new PrestaTypeMission();
+            $mission->user_id = Auth::id();
+            $mission->type_prestation_id = $id;
+
+            $missionDejaSelectionee = PrestaTypeMission::where('type_prestation_id', $id)->where('user_id', Auth::id())->first();
+
+            if (!$missionDejaSelectionee) {
+                if($mission->save()) {
+                    $missionCount++;
+                }
+            } elseif ($missionDejaSelectionee) {
+                $missionCount++;
+            }
+
+
+        }
+
+// Si toutes les missions ont été ajoutées avec succès, attribuer le rôle
+        if($missionCount == count($typePrestationIds)) {
+            $roleUser = new Role_user;
+            $roleUser->role_id = 4;
+            $roleUser->user_id = Auth::id();
+            $roleUser->save();
+
+            return 'Votre choix de prestation a bien été enregistré';
+        }
+
+        return "Une erreur s'est produite lors de l'ajout des missions et de l'attribution du rôle.";
     }
 
 
